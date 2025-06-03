d3b39ebc160c4c21a05a5f10ca1bd39f243d9b34
easywlaker011 <versusanimalls@gmail.com>
2025-06-03 15:40:30 +0500

    asd

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,20 @@ import jwt from "jsonwebtoken";
 
 
 // middleware to protect routes
-export const protectRoute = async (req,res, next) => {
+export const protectRoute = async (req, res, next) => {
   try {
     const token = req.headers.token;
+    if (!token) return res.status(401).json({ success: false, message: "Token missing" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
 
-    if(!user) return res.json({success:false, message: "User not found"})
-        req.user = user;
-       next();
+    if (!user) return res.status(404).json({ success: false, message: "User not found" });
+
+    req.user = user;
+    next();
   } catch (error) {
     console.log(error.message);
-    res.json({success:false, message: error.message})
+    return res.status(401).json({ success: false, message: "Invalid or expired token" });
   }
-}
+};
