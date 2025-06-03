619e7a255c4ab265e9513feb21bb24f334c574fc
easywlaker011 <versusanimalls@gmail.com>
2025-06-03 15:28:09 +0500

    ASD

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.use("/api/messages", messageRouter)
 //connect mongo db
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, ()=> console.log("server is running on port: " + PORT));
